008fb659159bd998791fd90366a08fde7ac8a229
bedo <bedirhansulvac@gmail.com>
2025-07-22 21:47:51 +0300

    Pracs

--- src/Test.java
+++ src/Test.java
@@ -2,31 +2,18 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        int number, total = 0, per = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        number = input.nextInt();
-        while (number <= 0) {
-            System.out.println("Sayı 0'dan büyük olmalı. Sayıyı Tekrar Giriniz: ");
-            number = input.nextInt();
+        String[] isimler = {"Fuat", "Sadık", "Nihat", "Bedo", "Batu"};
+        int toplam = 0;
 
+        for (int i = 0; i < isimler.length; i++) {
+            System.out.print(isimler[i] + " için IQ girin: ");
+            int iq = input.nextInt();
+            toplam += iq;
         }
 
-        for (int i = 1; i <= number; i++) {
-            if ((i % 3 == 0) && (i % 4 == 0)) {
-                total += i;
-                per++;
-
-            }
-        }
-        if (per > 0) {
-            int avg = (int) total / per;
-            System.out.println("3 ve 4'e tam bölünen sayıların ortalaması: " + avg);
-        } else {
-            System.out.println("0'dan" + number + "'ye kadar 3 ve 4'e tam bölünen sayı yok.");
-        }
-
-
+        double ortalama = (double) toplam / isimler.length;
+        System.out.println("Girilen IQ'ların ortalaması: " + ortalama);
     }
 }
